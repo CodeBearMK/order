12b98fb621190e9fa06ed6d47b3388d74a852868
cat32tw <mike1993xd@gmail.com>
2022-03-01 22:38:29 +0800

    3C購物中心

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace order
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(50, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Apple 系列產品";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox1.Location = new System.Drawing.Point(54, 98);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(202, 27);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "iPad 2 ($12,500元)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox2.Location = new System.Drawing.Point(54, 144);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(233, 27);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "iPad Mini ($10,500元)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox3.Location = new System.Drawing.Point(54, 191);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(224, 27);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Text = "iPhone 5 ($21,900元)";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(356, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "數量";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(360, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "結帳";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightGray;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(24, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(461, 57);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 323);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "3C購物中心";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace order
+{
+
+    public partial class Form1 : Form
+    {
+        int total = 0;
+        int qty = 1;
+        string item;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                qty = int.Parse(textBox1.Text);
+            }
+            catch
+            {
+                label3.Text = "";
+                MessageBox.Show("請輸入正整數值！", "錯誤訊息", MessageBoxButtons.OK);
+                textBox1.Text = "1";
+                return;
+            }
+
+            if (qty <= 0)
+            {
+                label3.Text = "";
+                MessageBox.Show("不能為負整數值！", "錯誤訊息", MessageBoxButtons.OK);
+                textBox1.Text = "1";
+                return;
+            }
+
+            if (checkBox1.Checked) total = 12500 * qty;
+            if (checkBox2.Checked) total = 10500 * qty;
+            if (checkBox3.Checked) total = 21900 * qty;
+            label3.Text = "你購買 " + item + "" + qty.ToString() + " 台 共計:NT$" + total.ToString("#,#") + "元";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                item = "iPad 2";
+                label3.Text = "";
+                checkBox1.AutoCheck = false;
+                checkBox2.AutoCheck = true;
+                checkBox3.AutoCheck = true;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                textBox1.Focus();
+                textBox1.Text = "1";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                item = "iPad Mini";
+                label3.Text = "";
+                checkBox2.AutoCheck = false;
+                checkBox1.AutoCheck = true;
+                checkBox3.AutoCheck = true;
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+                textBox1.Focus();
+                textBox1.Text = "1";
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                item = "iPhone 5";
+                label3.Text = "";
+                checkBox3.AutoCheck = false;
+                checkBox1.AutoCheck = true;
+                checkBox2.AutoCheck = true;
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                textBox1.Focus();
+                textBox1.Text = "1";
+            }
+        }
+    }
+}
